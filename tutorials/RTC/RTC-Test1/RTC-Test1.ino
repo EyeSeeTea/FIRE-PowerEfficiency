704e798503f35f2bee1e069109ff65629f3e152a
tgillett <tgillett@gmail.com>
2016-12-16 06:25:14 +1000

    Added initial time/date display

--- tutorials/RTC/RTC-Test1/RTC-Test1.ino
+++ tutorials/RTC/RTC-Test1/RTC-Test1.ino
@@ -15,19 +15,19 @@
 // You may set the data on the RTC chip, or just to display the data. 
 // Set the values below to true or flase as required.
 #define SETTDD    false	// Set Time, Date and Day of Week
-#define SETTIME   true	// Set Time
-#define SETDATE   true	// Set Date
+#define SETTIME   false	// Set Time
+#define SETDATE   false	// Set Date
 #define SETALRM1  true	// Set Alarm1
 #define SETALRM2  true	// Set Alarm2
 
 // Set the required time/date and daily alarm times to be set in the RTC.
-#define HH  17	// Hours
-#define MM  27	// Minutes
+#define HH  05	// Hours
+#define MM  52	// Minutes
 #define SS  02	// Seconds
-#define DD  15	// Date
+#define DD  16 	// Date
 #define MO  12	// Month
 #define YY  16	// Year (20yy)
-#define DAY Th	// Day of Week 
+#define DAY Fr	// Day of Week 
 
 #define AL1_HH 12 // Alarm1 Hours
 #define AL1_MM 05 // Alarm1 Minutes
@@ -57,6 +57,15 @@ void setup()
 {
   Wire.begin();        // Initialise I2C
   Serial.begin(9600);  // Initialise Serial Port
+
+  /* Display the current data */
+  Serial.print("RTC data : ");
+  displayTime();  // display the real-time clock data on the Serial Monitor
+  Serial.print("RTC Alarm1 : ");
+  display_alarm1(); // Display time
+  Serial.print("\RTC Alarm2 : ");
+  display_alarm2(); // Display time
+  Serial.println("\n---------------");
 
   /* Set time, day, date */
   if(SETTDD)
@@ -94,7 +103,7 @@ void setup()
   }
 
   /* Display the time and date */
-  Serial.println();
+  Serial.println("---------------");
   
   Serial.print("RTC data : ");
   displayTime();  // display the real-time clock data on the Serial Monitor
